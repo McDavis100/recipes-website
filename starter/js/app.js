e20b8659e77adb3d80bb169a235ddd4f26dad787
McDavis100 <mcdaviscairo100@gmail.com>
2022-11-24 16:53:29 +0200

    first commit

---
+++ starter/js/app.js
@@ -0,0 +1,18 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+  if (element) return element
+  throw Error(`There is some kind of error in names ${selector} does'nt 
+  exist)`
+  )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click',()=>{
+  links.classList.toggle('show-links')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
